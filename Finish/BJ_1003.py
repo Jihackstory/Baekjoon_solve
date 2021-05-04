c25e16dd6b3245da7c123ccf26bc8d60eb275953
Jihackstory <dlwlgkr159@naver.com>
2021-05-04 22:22:43 +0900

    'update'

--- Finish/BJ_1003.py
+++ Finish/BJ_1003.py
@@ -2,27 +2,10 @@
 # 다이나믹 프로그래밍
 # 실버 3
 
-
+###############
+# version 1
+###############
 import sys
-
-
-def pibonacci(number, init_count):
-    count = [] + init_count
-    if number == 0:
-        # print("0이 출력됩니다.")
-        count[0] += 1
-        return 0, count
-    elif number == 1:
-        # print("1이 출력됩니다.")
-        count[1] += 1
-        return 1, count
-    else:
-        pibo_num1, count1 = pibonacci(number - 1, count)
-        pibo_num2, count2 = pibonacci(number - 2, count)
-        count[0] += count1[0] + count2[0]
-        count[1] += count1[1] + count2[1]
-
-        return pibo_num1 + pibo_num2, count
 
 
 def check_count(number):
@@ -37,15 +20,29 @@ def check_count(number):
 
     return count_0, count_1
 
+
 data = []
 n = int(sys.stdin.readline())
 for i in range(n):
     data.extend(list(map(int, sys.stdin.readline().split())))
 
 for idx in range(n):
-    # init_count = [0, 0]
-    # pibo_num, count_tmp = pibonacci(data[idx], init_count)
-    # print(count_tmp[0], count_tmp[1])
+
     count_0, count_1 = check_count(data[idx])
     print(count_0, count_1)
 
+###############
+# version 2
+###############
+# import sys
+#
+# n = int(sys.stdin.readline())
+# dp = [[1, 0], [0, 1]]
+# data = [int(sys.stdin.readline()) for _ in range(n)]
+#
+# # 미리 만들어 두기 Dynamic programing
+# for i in range(2, max(data) + 1):
+#     dp.append([dp[i-2][0]+dp[i-1][0], dp[i-2][1]+dp[i-1][1]])
+#
+# for i in data:
+#     print(dp[i][0], dp[i][1])
--- Finish/BJ_1004.py
+++ Finish/BJ_1004.py
@@ -16,12 +16,13 @@ for i in range(n):
 for i in range(n):
     count = 0
     x1, y1, x2, y2 = data[i][0]
-    for j in range(1,len(data[i])):
+    for j in range(1, len(data[i])):
 
-        c_x,c_y, c_r = data[i][j]
+        c_x, c_y, c_r = data[i][j]
         check_1 = (x1 - c_x) ** 2 + (y1 - c_y) ** 2
         check_2 = (x2 - c_x) ** 2 + (y2 - c_y) ** 2
 
+        # XOR 연산
         if (check_1 < c_r ** 2) ^ (check_2 < c_r ** 2):
             count += 1
 
